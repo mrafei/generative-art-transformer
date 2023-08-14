5b9b83b887b6d34d518fec901e4d540f0dfadf45
mrafei <m.rafei96@gmail.com>
2023-08-14 17:35:13 +0330

    fix: make env available when ssr

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    JWT_SECRET: process.env.JWT_SECRET,
+    MONGODB_URI: process.env.MONGODB_URI
+  },
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
